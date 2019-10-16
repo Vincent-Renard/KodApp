20052bca353f87012a38d33ff5014d820b19f5c0
Vincent <vincent.renard@gmx.com>
2019-10-16 15:37:56 +0200

    DAO

---
+++ app/src/main/java/fr/univorleans/etu/renardez/kodapp/entities/daos/PositionUserDao.java
@@ -0,0 +1,31 @@
+package fr.univorleans.etu.renardez.kodapp.entities.daos;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import fr.univorleans.etu.renardez.kodapp.entities.PositionUser;
+
+@Dao
+public interface PositionUserDao {
+    String TABLE_NAME = "positions";
+
+    @Query("SELECT * FROM positions")
+    List<PositionUser> getAllPU();
+
+    @Query("SELECT * FROM positions WHERE id = :ids")
+    List<PositionUser> getSomePu(int... ids);
+
+    @Insert
+    int insertPos(PositionUser positionUser);
+
+    @Update
+    int updatePos(PositionUser positionUser);
+
+    @Query("DELETE FROM positions WHERE id = :posId")
+    int deleteItem(int posId);
+
+}
