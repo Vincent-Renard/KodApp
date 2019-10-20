7d0a10503a192684110d3c67453df9654bb29475
Romain Guidez <romain.guidez@etu.univ-orleans.fr>
2019-10-20 17:41:17 +0200

    Updated permission request for location

--- app/src/main/java/fr/univorleans/etu/renardez/kodapp/MainActivity.java
+++ app/src/main/java/fr/univorleans/etu/renardez/kodapp/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.univorleans.etu.renardez.kodapp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -9,10 +13,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
+    public static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     private LocationManager locationManager;
     private Location currentLocation;
@@ -21,32 +28,57 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
-    void getLocation() {
-        try {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        } catch (SecurityException e) {
-            e.printStackTrace();
+    public void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.i("LOCATION_REQUEST", "Permission previously refused");
+                final MainActivity self = this;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        new AlertDialog.Builder(self)
+                            .setMessage(R.string.request_permission_dialog)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ActivityCompat.requestPermissions(self, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+                                }
+                            })
+                            .create()
+                            .show();
+                    }
+                });
+            }
+            else {
+                Log.i("LOCATION_REQUEST", "Permission requested");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+            }
         }
+        else {
+            Log.i("LOCATION_REQUEST", "Permission already granted");
+            getLocation();
+        }
+    }
 
+    @SuppressLint("MissingPermission")
+    void getLocation() {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, this);
+        currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (currentLocation != null) {
+            Log.i("BUTTON_POS", "Lat " + currentLocation.getLatitude() + "Long " + currentLocation.getLongitude() + " Alt " + currentLocation.getAltitude());
+        }
     }
 
     public void clickPos(View view) {
-        //TODO
-
-        getLocation();
-        Log.i("BUTTON_POS", "Lat " + currentLocation.getLatitude() + "Long " + currentLocation.getLongitude() + " Alt " + currentLocation.getAltitude());
-
+        requestLocationPermission();
     }
 
     @Override
     public void onLocationChanged(Location location) {
         currentLocation = location;
-        // Log.i("BUTTON_POS", "Lat " + currentLocation.getLatitude() + "Long " + currentLocation.getLongitude() + " Alt " + currentLocation.getAltitude());
-
     }
 
     @Override
@@ -62,5 +94,29 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                }
+                else {
+                    final MainActivity self = this;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            new AlertDialog.Builder(self)
+                                .setMessage(R.string.permission_refused)
+                                .setPositiveButton(R.string.ok, null)
+                                .create()
+                                .show();
+                        }
+                    });
+                }
+                break;
+        }
     }
 }
