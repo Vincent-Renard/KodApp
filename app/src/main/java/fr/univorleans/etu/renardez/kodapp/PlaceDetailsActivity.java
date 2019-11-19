d27601313c73e6d548d757d971b70e5665bb9cd2
Vincent <vincent.renard@gmx.com>
2019-11-19 17:00:42 +0100

    + Detail Activity and layout

---
+++ app/src/main/java/fr/univorleans/etu/renardez/kodapp/PlaceDetailsActivity.java
@@ -0,0 +1,14 @@
+package fr.univorleans.etu.renardez.kodapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlaceDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
+    }
+}
