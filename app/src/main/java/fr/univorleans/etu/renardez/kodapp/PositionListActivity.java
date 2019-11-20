cf814bea995bb92de0a5eeff00ac6a99aef27942
Romain Guidez <romain.guidez@etu.univ-orleans.fr>
2019-11-20 16:27:23 +0100

    Remove old fragments

--- app/src/main/java/fr/univorleans/etu/renardez/kodapp/PositionListActivity.java
+++ app/src/main/java/fr/univorleans/etu/renardez/kodapp/PositionListActivity.java
@@ -1,33 +1,13 @@
 package fr.univorleans.etu.renardez.kodapp;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import fr.univorleans.etu.renardez.kodapp.db.Frigo;
-import fr.univorleans.etu.renardez.kodapp.entities.PositionUser;
-import fr.univorleans.etu.renardez.kodapp.entities.daos.PositionUserDao;
-
 public class PositionListActivity extends AppCompatActivity {
-    private Frigo base;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_position_list);
-        base = Frigo.getInstance(getApplicationContext());
-    }
-
-    public void populatePositionList() {
-        AsyncTask.execute(new Runnable() {
-            @Override
-            public void run() {
-                PositionUserDao dao = base.positionUserDao();
-                for (PositionUser pu : dao.getAllPU()) {
-                    //positionList.
-                }
-            }
-        });
     }
 }
