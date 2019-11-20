e2aa950ded840a825adb6366a0fd8457058d3387
Romain Guidez <romain.guidez@etu.univ-orleans.fr>
2019-11-20 15:23:01 +0100

    Add fragments to position activity

---
+++ app/src/main/java/fr/univorleans/etu/renardez/kodapp/PositionDisplayFragment.java
@@ -0,0 +1,18 @@
+package fr.univorleans.etu.renardez.kodapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PositionDisplayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_position_display, container, false);
+    }
+}
--- app/src/main/java/fr/univorleans/etu/renardez/kodapp/PositionListActivity.java
+++ app/src/main/java/fr/univorleans/etu/renardez/kodapp/PositionListActivity.java
@@ -4,7 +4,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import fr.univorleans.etu.renardez.kodapp.db.Frigo;
 import fr.univorleans.etu.renardez.kodapp.entities.PositionUser;
@@ -13,14 +12,11 @@ import fr.univorleans.etu.renardez.kodapp.entities.daos.PositionUserDao;
 public class PositionListActivity extends AppCompatActivity {
     private Frigo base;
 
-    private RecyclerView positionList;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_position_list);
         base = Frigo.getInstance(getApplicationContext());
-        positionList = findViewById(R.id.position_list);
     }
 
     public void populatePositionList() {
@@ -29,7 +25,7 @@ public class PositionListActivity extends AppCompatActivity {
             public void run() {
                 PositionUserDao dao = base.positionUserDao();
                 for (PositionUser pu : dao.getAllPU()) {
-                    positionList.
+                    //positionList.
                 }
             }
         });
